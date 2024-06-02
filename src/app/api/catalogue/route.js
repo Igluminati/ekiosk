b1681cb5112fc396a73b59beadbc7bb039360f6f
Igli Kristo <IgliKristo@protonmail.com>
2024-06-02 21:03:10 +0100

    Added GET and POST request handlers to fetch and create catalogue items using Prisma

--- src/app/api/catalogue/route.js
+++ src/app/api/catalogue/route.js
@@ -1,11 +1,22 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 /**
- * Singleton instance of the Prisma Client for interacting with the database.
- * @type {PrismaClient}
+ * Handles GET requests to fetch all items.
+ * 
+ * @returns {Promise<NextApiResponse>} A promise that resolves with the Next.js API route response object.
  */
-const prisma = new PrismaClient();
+export async function GET() {
+  try {
+    const items = await prisma.catalogueItem.findMany();
+    return NextResponse.json(items);
+  } catch (error) {
+    console.error('Error fetching items:', error);
+    return NextResponse.json({ message: 'Failed to fetch items.', error }, { status: 500 });
+  }
+}
 
 /**
  * Handles POST requests to create a new item.
@@ -15,28 +26,15 @@ const prisma = new PrismaClient();
  */
 export async function POST(req) {
   try {
-    /**
-     * Parses the request body as JSON and returns the parsed data.
-     * 
-     * @throws {SyntaxError} If the request body is not valid JSON.
-     * @returns {Object} The parsed JSON data from the request body.
-     */
     const data = await req.json();
 
-    /**
-     * Creates a new item in the database using the Prisma client.
-     * 
-     * @param {Object} data - The data to use for creating the new item.
-     * @returns {Promise<import("@prisma/client").Item>} A promise that resolves with the newly created item object.
-     * @throws {Prisma.PrismaClientKnownRequestError} An error thrown by Prisma if the create operation fails.
-     */
-    const newItem = await prisma.item.create({
-      data: data, // Use data from request body
+    const newItem = await prisma.catalogueItem.create({
+      data: data,
     });
     console.log('New item created:', newItem);
     return NextResponse.json({ message: 'Item created successfully!' });
   } catch (error) {
     console.error('Error creating item:', error);
-    return NextResponse.json({ message: 'Failed to create item.', error }, { status: 400 }); // Send error response
+    return NextResponse.json({ message: 'Failed to create item.', error }, { status: 400 });
   }
 }
